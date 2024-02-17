880e5b65fe197e6c31c830c8a7e41638c760e370
Anton Danilov <danilovantonne@gmail.com>
2024-02-17 23:47:02 +0300

    right parsing of str to long

--- src/main/java/ru/egpt/core/dao/tts/TTSDaoImpl.java
+++ src/main/java/ru/egpt/core/dao/tts/TTSDaoImpl.java
@@ -32,15 +32,16 @@ public class TTSDaoImpl implements TTSDao {
   @Override
   @MeasureTime("TTS")
   public InputStream getAudio(HttpHeaders headers, String text) {
+    long speakerId = Long.parseLong(
+        Objects.requireNonNull(
+            headers.getFirst("speaker_id"),
+            "\"speaker_id\" of header is null"
+        )
+    );
     headers.set("Content-Type", MediaType.APPLICATION_JSON_VALUE);
     TTSDtoRq rq = new TTSDtoRq();
     rq.setText(text);
-
-    log.info("Headers: {}", headers);
-    log.info("First: {}", headers.getFirst("speaker_id"));
-    log.info("Long value: {}", Long.getLong(headers.getFirst("speaker_id")));
-
-    rq.setSpeakerId(Long.getLong(headers.getFirst("speaker_id")));
+    rq.setSpeakerId(speakerId);
     HttpEntity<TTSDtoRq> requestEntity = new HttpEntity<>(rq, headers);
     log.info("TTS Rq: {}", requestEntity);
     ResponseEntity<Resource> responseEntity = restTemplate.exchange(endpoint, HttpMethod.POST, requestEntity, Resource.class);

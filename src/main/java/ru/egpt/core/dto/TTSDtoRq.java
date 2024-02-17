ec832a1681d340771d305c79eece518cc31b80c0
Anton Danilov <danilovantonne@gmail.com>
2024-02-17 23:32:09 +0300

    add tostring for TTSDtoRq

--- src/main/java/ru/egpt/core/dto/TTSDtoRq.java
+++ src/main/java/ru/egpt/core/dto/TTSDtoRq.java
@@ -3,9 +3,11 @@ package ru.egpt.core.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class TTSDtoRq {
 
   @JsonProperty("speaker_id")
